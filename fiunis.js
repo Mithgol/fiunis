6cd669581766bc1df1064e7f8a8cde230c6fa0b9
Mithgol the Webmaster <getgit@mithgol.ru>
2014-08-01 23:18:02 +0400

    add encoder and simplify the decoder's call for `.slice`

--- fiunis.js
+++ fiunis.js
@@ -10,8 +10,19 @@ Fiunis.prototype.decode = function(text){
          return fragment;
       } else { // regex-captured fragment's index: 1, 3, 5...
          return Buffer(
-            fragment.slice(1, fragment.length - 1) + '-'
+            fragment.slice(1, -1) + '-'
          ).toString('utf7');
+      }
+   }).join('');
+};
+
+Fiunis.prototype.encode = function(text){
+   var UTF7 = Buffer(text, 'utf-7').toString();
+   return text.split( /(\+[A-Za-z01-9+\/]*-)/ ).map(function(fragment, idx){
+      if( idx % 2 === 0 ){ // simple string fragment's index: 0, 2, 4...
+         return fragment;
+      } else { // regex-captured fragment's index: 1, 3, 5...
+         return '&' + fragment.slice(0, -1) + ';';
       }
    }).join('');
 };
