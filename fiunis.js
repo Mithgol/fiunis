3b64d031a6ad7138a5ab8bd002cb045b777f6124
Mithgol the Webmaster <getgit@mithgol.ru>
2014-08-01 23:27:06 +0400

    add tests for the encoder, fix a fatal bug

--- fiunis.js
+++ fiunis.js
@@ -17,8 +17,9 @@ Fiunis.prototype.decode = function(text){
 };
 
 Fiunis.prototype.encode = function(text){
-   var UTF7 = Buffer(text, 'utf-7').toString();
-   return text.split( /(\+[A-Za-z01-9+\/]*-)/ ).map(function(fragment, idx){
+   return Buffer(text, 'utf-7').toString().split(
+      /(\+[A-Za-z01-9+\/]*-)/
+   ).map(function(fragment, idx){
       if( idx % 2 === 0 ){ // simple string fragment's index: 0, 2, 4...
          return fragment;
       } else { // regex-captured fragment's index: 1, 3, 5...
--- test/test.js
+++ test/test.js
@@ -2,12 +2,13 @@
 var assert = require('assert');
 var Fiunis = require('../');
 
+var lorem = [
+   'Lorem ipsum dolor sit amet, consectetur adipisicing elit, ',
+   'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+].join('');
+
 describe("Fidonet Unicode strings' decoder", function(){
    it('leaves some simple text untouched', function(){
-      var lorem = [
-         'Lorem ipsum dolor sit amet, consectetur adipisicing elit, ',
-         'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-      ].join('');
       assert.strictEqual(
          Fiunis.decode(lorem),
          lorem
@@ -22,6 +23,26 @@ describe("Fidonet Unicode strings' decoder", function(){
          'Видеоролик ' +
          '\u9802\u5C16\u5C0D\u6C7A\u4E4B\u7A7F\u8932\u5B50\u7BC7' +
          ' небезынтересен.'
+      );
+   });
+});
+
+describe("Fidonet Unicode strings' encoder", function(){
+   it('leaves some simple text untouched', function(){
+      assert.strictEqual(
+         Fiunis.encode(lorem),
+         lorem
+      );
+   });
+   it('encodes Chinese characters from Diatlo.Svejaque (July 2014)',
+   function(){
+      assert.strictEqual(
+         Fiunis.encode(
+            'The video ' +
+            '\u9802\u5C16\u5C0D\u6C7A\u4E4B\u7A7F\u8932\u5B50\u7BC7' +
+            ' is interesting.'
+         ),
+         'The video &+mAJcFlwNbHpOS3p/iTJbUHvH; is interesting.'
       );
    });
 });
