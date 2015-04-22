496b9e7ddbbe1367ba21bd19c98e9f439b5cb904
Mithgol the Webmaster <getgit@mithgol.ru>
2015-04-23 00:28:33 +0300

    dirty hacks for ashtuchkin/iconv-lite#73 and ashtuchkin/iconv-lite#96

--- fiunis.js
+++ fiunis.js
@@ -1,4 +1,5 @@
-require('iconv-lite').extendNodeEncodings();
+var iconvLite = require('iconv-lite');
+iconvLite.extendNodeEncodings();
 
 var Fiunis = function(){
    if (!(this instanceof Fiunis)) return new Fiunis();
@@ -14,15 +15,70 @@ Fiunis.prototype.decode = function(text){
    }).join('');
 };
 
-Fiunis.prototype.encode = function(text){
-   var base64string = Buffer(text, 'utf16be').toString('base64');
-   while(
-      base64string.length > 0 &&
-      base64string.charAt(base64string.length - 1) === '='
-   ){
-      base64string = base64string.slice(0, -1);
+Fiunis.prototype.encode = function(text, targetEncoding){
+   if( typeof targetEncoding === 'undefined' ){ // encode whole string
+      var base64string = Buffer(text, 'utf16be').toString('base64');
+      while(
+         base64string.length > 0 &&
+         base64string.charAt(base64string.length - 1) === '='
+      ){
+         base64string = base64string.slice(0, -1);
+      }
+      return '&+' + base64string + '-;';
    }
-   return '&+' + base64string + '-;';
+
+   // otherwise detect and render Fidonet Unicode substrings
+   if( !iconvLite.encodingExists(targetEncoding) ){
+      throw new Error(this.errors.UNKNOWN_ENCODING);
+   }
+   if( iconvLite.defaultCharSingleByte !== '?' ){
+      throw new Error(this.errors.ICONVLITE_TAINTED);
+   }
+
+   var primalBuffer = Buffer(text, targetEncoding);
+   if( primalBuffer.length !== text.length ){
+      throw new Error(this.errors.MULTIBYTE_ENCODING);
+   }
+   var zebra = primalBuffer.toString(targetEncoding).split( /(\?+)/ );
+   if( zebra.length < 2 ){ // zero non-encodable substrings
+      return primalBuffer;
+   } else primalBuffer = void 0;
+
+   var _here = this;
+   var remainingStr = text;
+   var collected = [];
+   zebra.forEach(function(zebraLine, IDX){
+      if( IDX % 2 === 0 ){ // encodable substring's index: 0, 2, 4...
+         collected = collected.concat([zebraLine]);
+         remainingStr = remainingStr.slice(zebraLine.length);
+      } else { // non-encodable substring's index: 1, 3, 5...
+         var srcZebra = remainingStr.slice(0, zebraLine.length).split(
+            /(\?+)/
+         ).map(function(srcLine, srcIDX){
+            if( srcIDX % 2 === 0 ){
+               // actual non-encodable substring's index: 0, 2, 4...
+               if( srcLine.length < 1 ) return srcLine;
+               return _here.encode(srcLine);
+            } else {
+               // encodable source defaultCharSingleByte index: 1, 3, 5...
+               return srcLine;
+            }
+         });
+         collected = collected.concat(srcZebra);
+         remainingStr = remainingStr.slice(zebraLine.length);
+      }
+   });
+   return Buffer(collected.join(''), targetEncoding);
+};
+
+Fiunis.prototype.errors = {
+   UNKNOWN_ENCODING: 'The given encoding is unknown!',
+   MULTIBYTE_ENCODING: 'The given encoding is not a single-byte encoding!',
+   ICONVLITE_TAINTED: [
+      'The issue ',
+      'https://github.com/ashtuchkin/iconv-lite/issues/96',
+      ' is possible!'
+   ].join('')
 };
 
 module.exports = new Fiunis();
