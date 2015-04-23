7c964203abbf17ee4c7c21819d9f5e3388600b0f
Mithgol the Webmaster <getgit@mithgol.ru>
2015-04-23 08:25:28 +0300

    v2.1.0: has an encoding-aware generator of Fidonet Unicode substrings

--- test/test.js
+++ test/test.js
@@ -2,12 +2,14 @@
 var assert = require('assert');
 var Fiunis = require('../');
 
+require('iconv-lite').extendNodeEncodings();
+
 var lorem = [
    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, ',
    'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
 ].join('');
 
-describe("Fidonet Unicode strings' decoder", function(){
+describe('the decoder of Fidonet Unicode substrings', function(){
    it('leaves some simple text untouched', function(){
       assert.strictEqual(
          Fiunis.decode(lorem),
@@ -31,10 +33,16 @@ describe("Fidonet Unicode strings' decoder", function(){
          'буки'
       );
    });
+   it('decodes an empty string to an empty string', function(){
+      assert.strictEqual(
+         Fiunis.decode(''),
+         ''
+      );
+   });
 });
 
-describe("Fidonet Unicode strings' encoder", function(){
-   it('does not leave even simple text untouched', function(){
+describe('the generator of Fidonet Unicode substrings', function(){
+   it('can encode the whole text (even its ASCII characters)', function(){
       assert.notStrictEqual(
          Fiunis.encode(lorem),
          lorem
@@ -53,6 +61,24 @@ describe("Fidonet Unicode strings' encoder", function(){
       assert.strictEqual(
          Fiunis.encode('буки'),
          '&+BDEEQwQ6BDg-;'
+      );
+   });
+   it('encodes an empty string to an empty string', function(){
+      assert.strictEqual(
+         Fiunis.encode(''),
+         ''
+      );
+   });
+   it('encodes an example from ashtuchkin/iconv-lite#73 to CP866', function(){
+      assert.strictEqual(
+         Fiunis.encode('Хлѣбъ です。', 'cp866').toString('cp866'),
+         'Хл' + Fiunis.encode('ѣ') + 'бъ ' + Fiunis.encode('です。')
+      );
+   });
+   it('encodes an example from ashtuchkin/iconv-lite#73 to CP437', function(){
+      assert.strictEqual(
+         Fiunis.encode('Хлѣбъ です。', 'cp437').toString('cp437'),
+         Fiunis.encode('Хлѣбъ') + ' ' + Fiunis.encode('です。')
       );
    });
 });
